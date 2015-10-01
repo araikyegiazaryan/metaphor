d0aebce72430027a64616a32b663250942b6bc8c
Alex Hernandez <info@alejandrohernandez.me>
2015-10-01 10:40:31 -0700

    Scaffold Updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,5 @@
 var elixir = require('laravel-elixir');
 
-// Get Bower Directory Path (elixir.json)
-var components = elixir.config.bowerDir;
-
-// Get Bower Package Paths
-var paths = {
-   'jquery': components + '/jquery/dist/',
-   'bootstrap': components + '/bootstrap/',
-}
-
-// Create App.css
 elixir(function(mix) {
   mix.sass('metaphor.scss');
 });
